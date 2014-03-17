bb869a264467c646e7ed6a44ea44eda3f2b62567
David Ilizarov <dilizarov@gmail.com>
2014-03-17 14:10:14 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,27 @@
+SF Food Truck Finder
+========
+Find food trucks in San Francisco
+
+http://foodtrucks-sf.herokuapp.com/
+
+![ScreenShot](/app/assets/images/screenshot.png)
+
+Tech Stack
+=========
+Frontend
+* Backbone.js
+* JavaScript/jQuery
+* HTML5/CSS3
+
+Backend
+* RoR
+
+Notes
+=========
+Data populated from San Francisco Data:
+http://datasf.org
+
+Mobile Food Facility table:
+https://data.sfgov.org/Permitting/Mobile-Food-Facility-Permit/rqzj-sfat
+
+Utilizes Google Maps JavaScript API v3
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodTruckFinder::Application.config.secret_token = '520c9ac237423a5b8e259ebfd3f8124cbef1623728a8f96b72ad1781b71e633c341714f2fe52a49b4d50f14373cd1724c23fbdefaceca9890c4b5fe88d2251da'
